e04450009fbe3803976c7a9c6bc86049bae069ad
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-03 10:59:18 +0100

    [#5] author parameter as an optional one

--- basics/example.rb
+++ basics/example.rb
@@ -2,7 +2,7 @@ class Article
   attr_reader :title, :body, :author, :created_at
   attr_accessor :likes, :dislikes
 
-  def initialize(title, body, author)
+  def initialize(title, body, author = nil)
     @title, @body, @author = title, body, author
     @created_at = Time.now
     @likes = 0
