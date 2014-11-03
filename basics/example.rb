144122c37718fd6bd6ef0ec339717163114035b8
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-03 10:56:53 +0100

    [#4] Added initialize method

--- basics/example.rb
+++ basics/example.rb
@@ -1,4 +1,11 @@
 class Article
   attr_reader :title, :body, :author, :created_at
   attr_accessor :likes, :dislikes
+
+  def initialize(title, body, author)
+    @title, @body, @author = title, body, author
+    @created_at = Time.now
+    @likes = 0
+    @dislikes = 0
+  end
 end
