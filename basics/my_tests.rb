fc501e973e26c812fd4f724f989c04d304f1fd9e
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-04 09:59:57 +0100

    [#17] Added setup method

---
+++ basics/my_tests.rb
@@ -0,0 +1,44 @@
+require 'minitest/autorun'
+require './example'
+
+class ArticleTest < Minitest::Test
+  def setup
+    @article = Article.new('title', 'body')
+  end
+
+  def test_initialization
+  end
+
+  def test_initialization_with_anonymous_author
+  end
+
+  def test_liking
+  end
+
+  def test_disliking
+  end
+
+  def test_points
+  end
+
+  def test_long_lines
+  end
+
+  def test_truncate
+  end
+
+  def test_truncate_when_limit_is_longer_then_body
+  end
+
+  def test_truncate_when_limit_is_same_as_body_length
+  end
+
+  def test_length
+  end
+
+  def test_votes
+  end
+
+  def test_contain
+  end
+end
