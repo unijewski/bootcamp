8a32a17cc0af90a42b15dba50d9817321bf85421
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-05 12:04:17 +0100

    [#31] Validation for PlaceRent

--- app/models/place_rent.rb
+++ app/models/place_rent.rb
@@ -1,4 +1,9 @@
 class PlaceRent < ActiveRecord::Base
   belongs_to :car
   belongs_to :parking
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :parking_id, presence: true
+  validates :car_id, presence: true
 end
