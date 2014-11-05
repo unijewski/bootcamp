d55ea97330fb5372b1178614a7d52ebb51bc18f3
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-05 12:09:50 +0100

    [#30] Validation for Parking

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -1,5 +1,14 @@
 class Parking < ActiveRecord::Base
+  KIND_TYPES = %w(outdoor indoor private street)
+
   belongs_to :address
   belongs_to :owner, class_name: 'Person'
   has_many :place_rents
+
+  validates :places, presence: true
+  validates :hour_price, presence: true,
+                         numericality: true
+  validates :date_price, presence: true,
+                         numericality: true
+  validates :kind, inclusion: { in: KIND_TYPES }
 end
