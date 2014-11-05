73b1844eb88f5ada428e95cae2f3c1663d426079
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-05 16:50:56 +0100

    [#29] Added tests for Person model

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should save person with all parameters' do
+    person = people(:steve)
+    assert person.save
+  end
+
+  test 'should not save person with first_name parameter' do
+    person = Person.new(last_name: 'last_name')
+    assert_not person.save
+  end
+
+  test 'should save when only first_name is given' do
+    person = Person.new(first_name: 'first_name')
+    assert person.save
+  end
 end
