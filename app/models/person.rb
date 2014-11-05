7ff42a3d6c676e05030a97879a87d93fef086c11
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-05 12:02:46 +0100

    [#29] Validation for Person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,6 @@
 class Person < ActiveRecord::Base
-  has_many :cars
-  has_many :parkings
+  has_many :cars, foreign_key :owner_id
+  has_many :parkings, foreign_key :owner_id
+
+  validates :first_name, presence: true
 end
