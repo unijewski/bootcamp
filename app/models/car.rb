60b10b8cf42331e43c67c9d59712e727cb9b39c9
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-05 12:40:22 +0100

    [#26] Missing association in Car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,6 @@
 class Car < ActiveRecord::Base
   belongs_to :owner, class_name: 'Person'
+  has_many :place_rents
 
   validates :registration_number, presence: true
   validates :model, presence: true
