ecd18922da4036c172e4b1c82b5c4cae3297ee90
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-05 16:45:56 +0100

    [#28] Added tests for Car model

--- test/models/car_test.rb
+++ test/models/car_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class CarTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should save car with all parameters' do
+    car = cars(:bmw)
+    assert car.save
+  end
+
+  test 'should not save car with no parameters' do
+    car = Car.new
+    assert_not car.save
+  end
+
+  test 'should not save when one of parameters is not given' do
+    car = Car.new(model: 'model', registration_number: 'number')
+    assert_not car.save
+  end
 end
