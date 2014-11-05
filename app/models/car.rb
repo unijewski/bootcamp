19fd7d839df28cb5454c094abde76dd74b1f6bd3
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-05 12:00:11 +0100

    [#28] Validation for Car

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,7 @@
 class Car < ActiveRecord::Base
   belongs_to :owner, class_name: 'Person'
+
+  validates :registration_number, presence: true
+  validates :model, presence: true
+  validates :owner_id, presence: true
 end
