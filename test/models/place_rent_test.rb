52cedf9072a11585fe43113bda559a0280f6b67d
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-05 17:27:52 +0100

    [#31] Added tests for PlaceRent model

--- test/models/place_rent_test.rb
+++ test/models/place_rent_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class PlaceRentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should save place rent with all parameters' do
+    place_rent = place_rents(:bmw_at_renoma)
+    assert place_rent.save
+  end
+
+  test 'should not save place rent with no parameters' do
+    place_rent = PlaceRent.new
+    assert_not place_rent.save
+  end
+
+  test 'should not save when only one of parameters is given' do
+    place_rent = PlaceRent.new(start_date: Time.now)
+    assert_not place_rent.save
+  end
 end
