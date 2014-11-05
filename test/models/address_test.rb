91db5aaa7b5e1c0b7d7135fb73bef33dbe961439
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-05 17:43:21 +0100

    [#27] Added tests for Address model

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should save address with all parameters' do
+    address = addresses(:wroclaw)
+    assert address.save
+  end
+
+  test 'should not save address with no parameters' do
+    address = Address.new
+    assert_not address.save
+  end
+
+  test 'should not save when one of parameters is not given' do
+    address = Address.new(city: 'city', street: 'street')
+    assert_not address.save
+  end
+
+  test 'should not save when zip code is not valid' do
+    address = Address.new(city: 'city', street: 'street', zip_code: '123456')
+    assert_not address.save
+  end
 end
