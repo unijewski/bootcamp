418deae4ac0805a31601a84e0cf69830b7c660d9
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-05 12:58:06 +0100

    [#31] Validation refactor

--- app/models/place_rent.rb
+++ app/models/place_rent.rb
@@ -2,8 +2,5 @@ class PlaceRent < ActiveRecord::Base
   belongs_to :car
   belongs_to :parking
 
-  validates :start_date, presence: true
-  validates :end_date, presence: true
-  validates :parking_id, presence: true
-  validates :car_id, presence: true
+  validates :start_date, :end_date, :parking_id, :car_id, presence: true
 end
