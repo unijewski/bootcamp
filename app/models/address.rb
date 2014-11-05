569bc97ad3cad299c76dbac8f928039db2321e26
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-05 11:58:47 +0100

    [#27] Validation for Address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,10 @@
 class Address < ActiveRecord::Base
+  ZIP_CODE_REGEX = /\A\d{2}-\d{3}\Z/
+
   has_one :parking
+
+  validates :city, presence: true
+  validates :street, presence: true
+  validates :zip_code, presence: true,
+                       format: { with: ZIP_CODE_REGEX }
 end
