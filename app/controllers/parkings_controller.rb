fff54e9bb5209c89743093605d319b23f04da608
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-06 10:59:15 +0100

    [#35] Added edit and update action for parkings controller

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -21,6 +21,20 @@ class ParkingsController < ApplicationController
     end
   end
 
+  def edit
+    @parking = Parking.find(params[:id])
+  end
+
+  def update
+    @parking = Parking.find(params[:id])
+
+    if @parking.update(parking_params)
+      redirect_to @parking
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def parking_params
