d823bbdb8e95a79b44967ae8678620e047a81415
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-06 19:08:35 +0100

    [#42] Added new and create action with the view

--- app/controllers/place_rents_controller.rb
+++ app/controllers/place_rents_controller.rb
@@ -6,4 +6,29 @@ class PlaceRentsController < ApplicationController
   def show
     @place_rent = PlaceRent.find(params[:id])
   end
+
+  def new
+    @place_rent = PlaceRent.new(parking: find_parking)
+  end
+
+  def create
+    @place_rent = PlaceRent.new(place_rent_params)
+    @place_rent.parking = find_parking
+
+    if @place_rent.save
+      redirect_to @place_rent
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def place_rent_params
+    params.require(:place_rent).permit(:start_date, :end_date, :car_id)
+  end
+
+  def find_parking
+    @parking = Parking.find(params[:parking_id])
+  end
 end
