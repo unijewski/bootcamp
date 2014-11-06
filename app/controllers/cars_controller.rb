19e0e25a9c572cb300adced57a16f516c08ecbb5
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-06 14:01:35 +0100

    [#40] Added RESTful actions

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -8,17 +8,44 @@ class CarsController < ApplicationController
   end
 
   def new
+    @car = Car.new
   end
 
   def create
+    @car = Car.new(car_params)
+    @car.owner = current_person
+
+    if @car.save
+      redirect_to @car
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @car = Car.find(params[:id])
   end
 
   def update
+    @car = Car.find(params[:id])
+
+    if @car.update(car_params)
+      redirect_to @car
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @car = Car.find(params[:id])
+    @car.destroy
+
+    redirect_to cars_path
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:registration_number, :model)
   end
 end
