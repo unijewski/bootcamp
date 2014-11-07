b7152cdcd03a793da33d609b55e38d821c239931
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-07 15:23:30 +0100

    [#31] Fixed placerent tests

--- test/models/place_rent_test.rb
+++ test/models/place_rent_test.rb
@@ -1,18 +1,35 @@
 require 'test_helper'
 
 class PlaceRentTest < ActiveSupport::TestCase
-  test 'should save place rent with all parameters' do
-    place_rent = place_rents(:bmw_at_renoma)
-    assert place_rent.save
+  def setup
+    @place_rent = place_rents(:bmw_at_renoma)
   end
 
-  test 'should not save place rent with no parameters' do
-    place_rent = PlaceRent.new
-    assert_not place_rent.save
+  test 'place_rent should be valid' do
+    assert @place_rent.valid?
   end
 
-  test 'should not save when only one of parameters is given' do
-    place_rent = PlaceRent.new(start_date: Time.now)
-    assert_not place_rent.save
+  test 'when place_rent has no start_date parameter' do
+    @place_rent.start_date = nil
+    assert_not @place_rent.valid?
+    assert @place_rent.errors.has_key?(:start_date)
+  end
+
+  test 'when place_rent has no end_date parameter' do
+    @place_rent.end_date = nil
+    assert_not @place_rent.valid?
+    assert @place_rent.errors.has_key?(:end_date)
+  end
+
+  test 'when place_rent has no parking parameter' do
+    @place_rent.parking = nil
+    assert_not @place_rent.valid?
+    assert @place_rent.errors.has_key?(:parking_id)
+  end
+
+  test 'when place_rent has no car parameter' do
+    @place_rent.car = nil
+    assert_not @place_rent.valid?
+    assert @place_rent.errors.has_key?(:car_id)
   end
 end
