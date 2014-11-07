b1a3e7c05f39f5cbfe30e611574317abdb088850
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-07 11:07:00 +0100

    [#47] User opens parking details test

--- test/integration/parkings_test.rb
+++ test/integration/parkings_test.rb
@@ -1,8 +1,17 @@
 require 'test_helper'
 
 class ParkingsTest < ActionDispatch::IntegrationTest
-  test "user opens parkings index" do
+  test 'user opens parkings index' do
     visit '/parkings'
     assert has_content? 'parkings'
+  end
+
+  test 'user opens parking details' do
+    visit 'parkings'
+    click_link('Show')
+    assert has_content? 'Places'
+    assert has_content? 'Kind'
+    assert has_content? 'Hour price'
+    assert has_content? 'Day price'
   end
 end
