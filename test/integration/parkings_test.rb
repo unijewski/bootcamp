1242fb72b53afef370aafa2b43f93d4d46678d65
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-07 10:55:50 +0100

    [#46] Working capybara

--- test/integration/parkings_test.rb
+++ test/integration/parkings_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 class ParkingsTest < ActionDispatch::IntegrationTest
   test "user opens parkings index" do
     visit '/parkings'
-    assert has_content? 'Parkings'
+    assert has_content? 'parkings'
   end
 end
