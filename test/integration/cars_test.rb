2d6f6c8bfb45b65ce00658383c3ec9467f96a27c
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-07 13:01:46 +0100

    [#49] User creates a car

--- test/integration/cars_test.rb
+++ test/integration/cars_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class CarsTest < ActionDispatch::IntegrationTest
+  def fill_in_the_form
+    fill_in 'Registration number', with: 'KR 1234A'
+    fill_in 'Model', with: 'BMW'
+  end
+
   test 'user opens cars index' do
     visit '/cars'
     assert has_content? 'Listing cars'
@@ -12,4 +17,16 @@ class CarsTest < ActionDispatch::IntegrationTest
     assert has_content? 'Registration number'
     assert has_content? 'Model'
   end
+
+  test 'user adds a new car' do
+    visit '/cars'
+    click_link 'New car'
+    fill_in_the_form
+    click_button 'Create Car'
+    assert has_content? 'Registration number: KR 1234A'
+    assert has_content? 'Model: BMW'
+    assert has_content? 'The car has been created!'
+  end
+
+
 end
