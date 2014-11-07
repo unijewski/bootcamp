4f4947b954e240b5ac088d55a2b6cead6c25fbe2
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-07 11:57:43 +0100

    [#47] User edits a parking test

--- test/integration/parkings_test.rb
+++ test/integration/parkings_test.rb
@@ -1,6 +1,16 @@
 require 'test_helper'
 
 class ParkingsTest < ActionDispatch::IntegrationTest
+  def fill_in_the_form
+    fill_in 'Places', with: '100'
+    select 'private', from: 'Kind'
+    fill_in 'Hour price', with: '3.50'
+    fill_in 'Day price', with: '25.00'
+    fill_in 'City', with: 'City'
+    fill_in 'Street', with: 'Street'
+    fill_in 'Zip code', with: '12-345'
+  end
+
   test 'user opens parkings index' do
     visit '/parkings'
     assert has_content? 'parkings'
@@ -15,16 +25,10 @@ class ParkingsTest < ActionDispatch::IntegrationTest
     assert has_content? 'Day price'
   end
 
-  test 'user add a new parking' do
+  test 'user adds a new parking' do
     visit '/parkings'
     click_link 'New parking'
-    fill_in 'Places', with: '100'
-    select 'private', from: 'Kind'
-    fill_in 'Hour price', with: '3.50'
-    fill_in 'Day price', with: '25.00'
-    fill_in 'City', with: 'City'
-    fill_in 'Street', with: 'Street'
-    fill_in 'Zip code', with: '12-345'
+    fill_in_the_form
     click_button 'Create Parking'
     assert has_content? 'Parking details'
     assert has_content? 'Places: 100'
@@ -33,5 +37,21 @@ class ParkingsTest < ActionDispatch::IntegrationTest
     assert has_content? 'Day price: 25.0'
     assert has_content? 'City: 12-345 City'
     assert has_content? 'Street: Street'
+    assert has_content? 'The parking has been created!'
+  end
+
+  test 'user edits a parking' do
+    visit '/parkings'
+    click_link 'Edit'
+    fill_in_the_form
+    click_button 'Update Parking'
+    assert has_content? 'Parking details'
+    assert has_content? 'Places: 100'
+    assert has_content? 'Kind: private'
+    assert has_content? 'Hour price: 3.5'
+    assert has_content? 'Day price: 25.0'
+    assert has_content? 'City: 12-345 City'
+    assert has_content? 'Street: Street'
+    assert has_content? 'The parking has been updated!'
   end
 end
