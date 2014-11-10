865102098b4cd134ccc8054b449ea4daea4dbcee
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-10 20:58:17 +0100

    [#52] Improved tests

--- test/models/parking_test.rb
+++ test/models/parking_test.rb
@@ -60,25 +60,41 @@ class ParkingTest < ActiveSupport::TestCase
   test 'when we query about public parkings' do
     parkings = Parking.public_parkings
     assert_equal 1, parkings.size
+    assert_equal 300, parkings.first.places
+    assert_equal 6.00, parkings.first.hour_price
+    assert_equal 30.00, parkings.first.day_price
+    assert_equal 'public', parkings.first.kind
   end
 
   test 'when we query about private parkings' do
     parkings = Parking.private_parkings
     assert_equal 1, parkings.size
+    assert_equal 600, parkings.first.places
+    assert_equal 3.00, parkings.first.hour_price
+    assert_equal 25.00, parkings.first.day_price
+    assert_equal 'private', parkings.first.kind
   end
 
   test 'when we query about parkings by day price' do
     parkings = Parking.by_day_price(20, 50)
     assert_equal 2, parkings.size
+    assert_equal [300, 600], parkings.map(&:places)
+    assert_equal ['public', 'private'], parkings.map(&:kind)
   end
 
   test 'when we query about parkings by hour price' do
     parkings = Parking.by_hour_price(1, 10)
     assert_equal 2, parkings.size
+    assert_equal [300, 600], parkings.map(&:places)
+    assert_equal ['public', 'private'], parkings.map(&:kind)
   end
 
   test 'when we query about parkings by city' do
     parkings = Parking.by_city('Wroclaw')
     assert_equal 1, parkings.size
+    assert_equal 600, parkings.first.places
+    assert_equal 3.00, parkings.first.hour_price
+    assert_equal 25.00, parkings.first.day_price
+    assert_equal 'private', parkings.first.kind
   end
 end
