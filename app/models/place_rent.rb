24b8f06a86fe2ac4fe3943a64251b1d84ec09f86
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-10 16:53:47 +0100

    [#50] Divided calculate method into smaller methods

--- app/models/place_rent.rb
+++ app/models/place_rent.rb
@@ -7,10 +7,24 @@ class PlaceRent < ActiveRecord::Base
   validates :start_date, :end_date, :parking, :car, presence: true
 
   def calculate_price
-    day_price = parking.day_price
-    hour_price = parking.hour_price
-    day_spent = (end_date - start_date).to_i / 1.day
-    hours_spent = (end_date - start_date).to_i / 1.hour
     self.price = (day_price * day_spent) + (hour_price * hours_spent)
+  end
+
+  private
+
+  def day_price
+    parking.day_price
+  end
+
+  def hour_price
+    parking.hour_price
+  end
+
+  def day_spent
+    (end_date - start_date).to_i / 1.day
+  end
+
+  def hours_spent
+    (end_date - start_date).to_i / 1.hour
   end
 end
