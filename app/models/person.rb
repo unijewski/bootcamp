598ce6a865bde6c1fd2aefbd90f94c2340d5d115
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-11 09:56:18 +0100

    [#58] Added full_name method

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,4 +3,12 @@ class Person < ActiveRecord::Base
   has_many :parkings, foreign_key: 'owner_id'
 
   validates :first_name, presence: true
+
+  def full_name
+    if last_name.present?
+      first_name + ' ' + last_name
+    else
+      first_name
+    end
+  end
 end
