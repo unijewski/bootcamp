8c1cab0946f7782d5d5b12f2f4ea13ab686d7b81
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-11 11:49:17 +0100

    [#58] Added unit tests for full_name method

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -14,4 +14,13 @@ class PersonTest < ActiveSupport::TestCase
     assert_not @person.valid?
     assert @person.errors.has_key?(:first_name)
   end
+
+  test 'when we display full name' do
+    assert_equal 'Steve Jobs', @person.full_name
+  end
+
+  test 'when we display full name if there is only a name' do
+    @person.last_name = nil
+    assert_equal 'Steve', @person.full_name
+  end
 end
