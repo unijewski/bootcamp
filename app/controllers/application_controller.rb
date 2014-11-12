4d1c97ae7190e2fdf46bc8835b5b1faa377244bb
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-12 17:12:09 +0100

    [#70] Switching between languages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def set_locale
-    I18n.locale = extract_locale_from_accept_language_header
+    I18n.locale = params[:locale] || extract_locale_from_accept_language_header
   end
 
   private
