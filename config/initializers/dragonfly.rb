b0ab7ac4fd255d6f2253adef4615e7c9c7a587b3
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-13 15:06:00 +0100

    [#83] Dragonfly initializer

---
+++ config/initializers/dragonfly.rb
@@ -0,0 +1,26 @@
+require 'dragonfly'
+
+# Configure
+Dragonfly.app.configure do
+  plugin :imagemagick
+
+  secret "7712fe569e7aa0665e12afea8b60d0ba7a0f659232974e2394a32a4c7dd503b6"
+
+  url_format "/media/:job/:name"
+
+  datastore :file,
+    root_path: Rails.root.join('public/system/dragonfly', Rails.env),
+    server_root: Rails.root.join('public')
+end
+
+# Logger
+Dragonfly.logger = Rails.logger
+
+# Mount as middleware
+Rails.application.middleware.use Dragonfly::Middleware
+
+# Add model functionality
+if defined?(ActiveRecord::Base)
+  ActiveRecord::Base.extend Dragonfly::Model
+  ActiveRecord::Base.extend Dragonfly::Model::Validations
+end
