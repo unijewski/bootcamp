91a1bb21cbe2dafbd3dd095c629f88a23ecc9bc7
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-14 12:00:31 +0100

    [#89] find_or_create_for_facebook method

--- app/models/facebook_account.rb
+++ app/models/facebook_account.rb
@@ -2,4 +2,19 @@ class FacebookAccount < ActiveRecord::Base
   belongs_to :person
 
   validates :uid, :person, presence: true
+
+  def self.find_or_create_for_facebook(auth_hash)
+    find_by(uid: auth_hash[:uid]) || FacebookAccount.create_facebook_account(auth_hash)
+  end
+
+  private
+
+  def self.create_facebook_account(auth_hash)
+    fb_account = FacebookAccount.new(uid: auth_hash[:uid])
+    fb_account.build_person
+    fb_account.person.first_name = auth_hash[:info][:first_name]
+    fb_account.person.last_name = auth_hash[:info][:last_name]
+    fb_account.save
+    fb_account
+  end
 end
