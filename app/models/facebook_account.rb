71367b7d586732164a9a34d24f3d55d5a6dd7114
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-11-14 16:05:08 +0100

    [#89] Code refactor for facebook_account model

--- app/models/facebook_account.rb
+++ app/models/facebook_account.rb
@@ -4,17 +4,13 @@ class FacebookAccount < ActiveRecord::Base
   validates :uid, :person, presence: true
 
   def self.find_or_create_for_facebook(auth_hash)
-    find_by(uid: auth_hash[:uid]) || FacebookAccount.create_facebook_account(auth_hash)
-  end
-
-  private
-
-  def self.create_facebook_account(auth_hash)
-    fb_account = FacebookAccount.new(uid: auth_hash[:uid])
-    fb_account.build_person
-    fb_account.person.first_name = auth_hash[:info][:first_name]
-    fb_account.person.last_name = auth_hash[:info][:last_name]
-    fb_account.save
-    fb_account
+    where(uid: auth_hash[:uid]).first_or_create do |account|
+      account.uid = auth_hash[:uid]
+      account.build_person(
+        first_name: auth_hash[:info][:first_name],
+        last_name: auth_hash[:info][:last_name]
+      )
+      account.save
+    end
   end
 end
